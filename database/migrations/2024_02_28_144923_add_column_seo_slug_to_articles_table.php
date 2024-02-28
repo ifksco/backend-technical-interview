a16f5d166a350db6ffdfcb97968322365523225c
cicada3301 <iglazunoff39@gmail.com>
2024-02-28 18:04:12 +0200

    added migrations

---
+++ database/migrations/2024_02_28_144923_add_column_seo_slug_to_articles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->string('seo_slug')->after('user_id');
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('total_articles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropIfExists('seo_slug');
+        });
+    }
+};
